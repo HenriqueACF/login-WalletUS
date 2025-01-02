142cf41e9653fad53193adcc44586c289abbf9a1
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2025-01-02 10:57:01 -0300

    first commit

---
+++ src/main/java/com/example/login_and_register_api/LoginAndRegisterApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.login_and_register_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegisterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegisterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/login_and_register_api/LoginAndRegisterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_and_register_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegisterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
