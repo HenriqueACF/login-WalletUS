8a930bc8d7f80669b7aaddd6c9524fe1f5418940
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2025-01-02 11:25:39 -0300

    feat: user entity

---
+++ src/main/java/com/example/login_and_register_api/domain/user/User.java
@@ -0,0 +1,22 @@
+package com.example.login_and_register_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String Id;
+    private String name;
+    private String password;
+}
