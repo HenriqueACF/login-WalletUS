53bc1019468dd806afd9f836b3358a05abe3a009
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2025-01-02 14:55:46 -0300

    feat: SecurityFilter

--- src/main/java/com/example/login_and_register_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_and_register_api/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.login_and_register_api.repositories;
 import com.example.login_and_register_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // ENTIDADE E TIPO ID
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
